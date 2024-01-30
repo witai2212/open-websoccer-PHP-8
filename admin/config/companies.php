b1663f052410b88c25e5799e0789f1424df05815
witai2212 <55430672+witai2212@users.noreply.github.com>
2024-01-30 21:16:11 +0100

    sponsor generator

---
+++ admin/config/companies.php
@@ -0,0 +1,13 @@
+<?php
+$construction = array("Neue Heimat","Philipp Holzmann","German Property Group","Signa Holding","Evergrande");
+
+$sponsors = array(
+	"REWE","Kömmerling","Bayer","Evonik Industries","Deutsche Postbank","Indeed","WWK",
+	"Telekom","Gazprom","Emirates","Heinz von Heiden","bet at home","Red Bull","Schwarzwaldmilch","Wiesenhof","SAP","Mercedes-Benz-Bank","Volkswagen",
+	"Enron","Comroad","Kmart","Swissair","Kirch-Gruppe","Arthur Andersen","WorldCom","Conseco","Parmalat","Heros Unternehmensgruppe","Yukos","Göttinger Gruppe","Northern Rock",
+	"Bear Stearns","Lehman Brothers","AIG","Washington Mutual","Royal Bank of Scotland","Sanlu","Nortel","Arcandor","Hypo Real Estate",
+	"Envio Recycling","Beluga Shipping","Harles und Jentzsch","Teldafax","Poly Implant Prothèse","MF Global","Schlecker","Müller-Brot","S&K-Gruppe","Mt.Gox","Infinus","pcfritz.de",
+	"Hypo Alpe Adria","Bankhaus Wölbern & Co.","Imtech","Maple Bank","Unister","Hanjin Shipping","Takata","P&R-Gruppe","Theranos","Sears","Pacific Gas & Electric","Purdue Pharma",
+	"Wilke Waldecker Fleischwaren","J. C. Penney","Wirecard","Commerzialbank Mattersburg","FTX","Credit Suisse");
+
+?>

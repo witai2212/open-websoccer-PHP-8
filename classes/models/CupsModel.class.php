08f6818003b57e7278a46f6916ecc9125d97f1db
Flaming Moe <55430672+witai2212@users.noreply.github.com>
2026-08-24 11:43:12 +0200

    Task 1001: expand national cup overview

--- classes/models/CupsModel.class.php
+++ classes/models/CupsModel.class.php
@@ -20,8 +20,15 @@
 
 ******************************************************/
 
+/*
+ * Task 1001 - National cup overview
+ * Revision 1 - 2026-08-24
+ */
+
+require_once __DIR__ . '/../services/InternationalCupPhaseHelper.class.php';
+
 /**
- * Provides names of cups and their rounds.
+ * Provides national cup overview data.
  */
 class CupsModel implements IModel {
 	private $_db;
@@ -48,18 +55,16 @@ class CupsModel implements IModel {
 	 */
 	public function getTemplateParameters() {
 		
-		// userId
 		$user = $this->_websoccer->getUser();
-	    $userId = $user->id;
+		$userId = $user->id;
 		
-		// user teamId
-		$teamId = $this->_websoccer->getUser()->getClubId($this->_websoccer, $this->_db);
+		$teamId = $user->getClubId($this->_websoccer, $this->_db);
 		if ($teamId < 1) {
 			throw new Exception($this->_i18n->getMessage("feature_requires_team"));
 		}
 		
 		$myCup = CupsDataService::getCupDataByTeamId($this->_websoccer, $this->_db, $teamId);
-		$cupId = $this->_websoccer->getRequestParameter('cup');
+		$cupId = (int) $this->_websoccer->getRequestParameter('cup');
 		$cups = CupsDataService::getCups($this->_websoccer, $this->_db);
 		
 		if ($cupId < 1 && isset($myCup['id'])) {
@@ -69,18 +74,39 @@ class CupsModel implements IModel {
 			$cupId = (int) $cups[0]['id'];
 		}
 		
-		$cup = ($cupId > 0) ? CupsDataService::getCupDataByCupId($this->_websoccer, $this->_db, $cupId) : array();
-		$matches = (isset($cup['name']) && strlen((string) $cup['name']))
-			? CupsDataService::getMatchesByCupname($this->_websoccer, $this->_db, $cup['name'])
-			: array();
+		$cup = ($cupId > 0)
+			? CupsDataService::getCupDataByCupId($this->_websoccer, $this->_db, $cupId)
+			: null;
 		
-		return array(
-			'cups' => $cups,
-			'cup' => $cup,
-			'matches' => $matches,
-			'match_round_groups' => MatchGroupingDataService::groupByRound($matches),
-			'user_id' => $userId
-		);
+		if ($cup && isset($cup['name']) && strlen((string) $cup['name'])) {
+			InternationalCupPhaseHelper::selectCurrentPhase(
+				$this->_websoccer,
+				$this->_db,
+				(string) $cup['name']
+			);
+			
+			$overview = EuropeanCupDataService::getCupOverview(
+				$this->_websoccer,
+				$this->_db,
+				(string) $cup['name'],
+				'cups',
+				'cups'
+			);
+		} else {
+			$overview = EuropeanCupDataService::getCupOverview(
+				$this->_websoccer,
+				$this->_db,
+				'',
+				'cups',
+				'cups'
+			);
+		}
+		
+		$overview['cups'] = $cups;
+		$overview['selected_cup_id'] = $cupId;
+		$overview['user_id'] = $userId;
+		
+		return $overview;
 	}
 	
 }

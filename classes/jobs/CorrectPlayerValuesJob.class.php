ae68462a6642bd208d8170dfb365e0684f41da55
Flaming Moe <55430672+witai2212@users.noreply.github.com>
2026-08-26 09:25:47 +0200

    Task 1011: replace obsolete player correction job

--- classes/jobs/CorrectPlayerValuesJob.class.php
+++
@@ -1,9 +0,0 @@
-<?php
-class CorrectPlayerValuesJob extends AbstractJob {
-    function execute() {
-        PlayersDataService::playerStrengthCorrection($this->_websoccer, $this->_db);
-        // Existing job remains the automatic engine. It processes the oldest 100 players per run.
-        MarketValueMaintenanceService::run($this->_websoccer, $this->_db, 'all', 0, false, 100, 0);
-    }
-}
-?>

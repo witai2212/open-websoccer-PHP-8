e170dc806c7d8c07c9fd33e4e1451dcab8feeee4
Flaming Moe <55430672+witai2212@users.noreply.github.com>
2026-09-01 09:54:38 +0200

    Task 1018: require completed predecessor building

--- classes/actions/OrderBuildingController.class.php
+++ classes/actions/OrderBuildingController.class.php
@@ -3,22 +3,24 @@
 
   This file is part of OpenWebSoccer-Sim.
 
-  OpenWebSoccer-Sim is free software: you can redistribute it 
-  and/or modify it under the terms of the 
-  GNU Lesser General Public License 
+  OpenWebSoccer-Sim is free software: you can redistribute it
+  and/or modify it under the terms of the
+  GNU Lesser General Public License
   as published by the Free Software Foundation, either version 3 of
   the License, or any later version.
 
   OpenWebSoccer-Sim is distributed in the hope that it will be
   useful, but WITHOUT ANY WARRANTY; without even the implied
-  warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+  warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
   See the GNU Lesser General Public License for more details.
 
-  You should have received a copy of the GNU Lesser General Public 
-  License along with OpenWebSoccer-Sim.  
+  You should have received a copy of the GNU Lesser General Public
+  License along with OpenWebSoccer-Sim.
   If not, see <http://www.gnu.org/licenses/>.
 
 ******************************************************/
+
+// CM23 | 2026-09-01 | Revision 1
 
 /**
  * Validates and stores a building order for the stadium environment.
@@ -27,42 +29,41 @@ class OrderBuildingController implements IActionController {
 	private $_i18n;
 	private $_websoccer;
 	private $_db;
-	
+
 	public function __construct(I18n $i18n, WebSoccer $websoccer, DbConnection $db) {
 		$this->_i18n = $i18n;
 		$this->_websoccer = $websoccer;
 		$this->_db = $db;
 	}
-	
+
 	/**
 	 * (non-PHPdoc)
 	 * @see IActionController::executeAction()
 	 */
 	public function executeAction($parameters) {
-		
+
 		$buildingId = $parameters['id'];
 		$user = $this->_websoccer->getUser();
 		$teamId = $user->getClubId($this->_websoccer, $this->_db);
 		if (!$teamId) {
 			throw new Exception($this->_i18n->getMessage("feature_requires_team"));
 		}
-		
+
 		$dbPrefix = $this->_websoccer->getConfig('db_prefix');
 		$result = $this->_db->querySelect('*', $dbPrefix . '_stadiumbuilding', 'id = %d', $buildingId);
 		$building = $result->fetch_array();
 		$result->free();
-		
+
 		if (!$building) {
-			// no i18n required since this should actually not happen if used properly.
 			throw new Exception('illegal building.');
 		}
-		
+
 		// check budget
 		$team = TeamsDataService::getTeamSummaryById($this->_websoccer, $this->_db, $teamId);
 		if ($team['team_budget'] <= $building['costs']) {
 			throw new Exception($this->_i18n->getMessage('stadiumenvironment_build_err_too_expensive'));
 		}
-		
+
 		// check if already exists in team
 		$result = $this->_db->querySelect('*', $dbPrefix . '_buildings_of_team', 'team_id = %d AND building_id = %d', array($teamId, $buildingId));
 		$buildingExists = $result->fetch_array();
@@ -70,27 +71,32 @@ class OrderBuildingController implements IActionController {
 		if ($buildingExists) {
 			throw new Exception($this->_i18n->getMessage('stadiumenvironment_build_err_already_exists'));
 		}
-		
-		// check required building
+
+		// A required predecessor must exist and its construction must be completed.
 		if ($building['required_building_id']) {
-			$result = $this->_db->querySelect('*', $dbPrefix . '_buildings_of_team', 'team_id = %d AND building_id = %d', array($teamId, $building['required_building_id']));
-			$requiredBuildingExists = $result->fetch_array();
+			$result = $this->_db->querySelect(
+				'*',
+				$dbPrefix . '_buildings_of_team',
+				'team_id = %d AND building_id = %d AND construction_deadline < %d',
+				array($teamId, $building['required_building_id'], $this->_websoccer->getNowAsTimestamp())
+			);
+			$requiredBuildingCompleted = $result->fetch_array();
 			$result->free();
-			
-			if (!$requiredBuildingExists) {
+
+			if (!$requiredBuildingCompleted) {
 				throw new Exception($this->_i18n->getMessage('stadiumenvironment_build_err_requires_building'));
 			}
 		}
-		
+
 		// check premium costs
 		if ($building['premiumfee'] > $user->premiumBalance) {
 			throw new Exception($this->_i18n->getMessage('stadiumenvironment_build_err_premium_balance'));
 		}
-		
+
 		// withdraw costs
-		BankAccountDataService::debitAmount($this->_websoccer, $this->_db, $teamId, $building['costs'], 
+		BankAccountDataService::debitAmount($this->_websoccer, $this->_db, $teamId, $building['costs'],
 			'building_construction_fee_subject', $building['name']);
-		
+
 		// place order
 		$constructionDeadline = $this->_websoccer->getNowAsTimestamp() + $building['construction_time_days'] * 24 * 3600;
 		$this->_db->queryInsert(array(
@@ -98,23 +104,22 @@ class OrderBuildingController implements IActionController {
 				'team_id' => $teamId,
 				'construction_deadline' => $constructionDeadline
 				), $dbPrefix . '_buildings_of_team');
-		
+
 		// withdraw premium fee
 		if ($building['premiumfee']) {
 			PremiumDataService::debitAmount($this->_websoccer, $this->_db, $user->id, $building['premiumfee'], "order-building");
 		}
-		
+
 		// fan popularity effects are applied only after construction has finished.
 		// See StadiumEnvironmentPlugin::applyCompletedFanPopularityBuildings().
-		
+
 		// success message
-		$this->_websoccer->addFrontMessage(new FrontMessage(MESSAGE_TYPE_SUCCESS, 
+		$this->_websoccer->addFrontMessage(new FrontMessage(MESSAGE_TYPE_SUCCESS,
 				$this->_i18n->getMessage("stadiumenvironment_build_success"),
 				""));
-		
+
 		return null;
 	}
-	
 }
 
 ?>

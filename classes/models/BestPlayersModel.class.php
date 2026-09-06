7a41ff1a3896f4b926dd7f42f656c27e50f31eb6
Flaming Moe <55430672+witai2212@users.noreply.github.com>
2026-09-06 13:31:04 +0200

    Task 1018: add watchlist status to best players

--- classes/models/BestPlayersModel.class.php
+++ classes/models/BestPlayersModel.class.php
@@ -50,6 +50,29 @@ class BestPlayersModel implements IModel {
 	    
 	    $bestplayers = PlayersDataService::getBestPlayersByStrength($this->_websoccer, $this->_db);
 	    
+	    // Attach watchlist status for the currently logged-in manager's club.
+	    // Transfer-list status is already part of the player data returned above.
+	    $watchlistPlayerIds = array();
+	    $teamId = $this->_websoccer->getUser()->getClubId($this->_websoccer, $this->_db);
+	    
+	    if ($teamId > 0) {
+	        $result = $this->_db->querySelect(
+	            'spieler_id',
+	            $this->_websoccer->getConfig('db_prefix') . '_watchlist',
+	            'verein_id = %d',
+	            $teamId
+	        );
+	        while ($watchlistEntry = $result->fetch_array()) {
+	            $watchlistPlayerIds[(int) $watchlistEntry['spieler_id']] = TRUE;
+	        }
+	        $result->free();
+	    }
+	    
+	    foreach ($bestplayers as $index => $player) {
+	        $playerId = isset($player['id']) ? (int) $player['id'] : 0;
+	        $bestplayers[$index]['on_watchlist'] = ($playerId > 0 && isset($watchlistPlayerIds[$playerId])) ? 1 : 0;
+	    }
+	    
 		return array("players" => $bestplayers);
 	}
 	
